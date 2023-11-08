561824cb9ffdcef74f5e839e29e8da80a5e8cc69
Jose Serra <148002646+jofran93@users.noreply.github.com>
2023-11-08 02:10:29 -0300

    Delete TuPrimeraPagina+Serra/TuPrimeraPagina-Serra directory

--- TuPrimeraPagina+Serra/TuPrimeraPagina-Serra/TuPrimeraPagina/miapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.pagina_de_inicio, name='pagina_de_inicio'),
-    path('crear_cliente/', views.crear_cliente, name='cliente'),
-    path('crear_producto/', views.crear_producto, name='producto'),
-    path('crear_empleado/', views.crear_empleado, name='empleado')
-]
